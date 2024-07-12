f4970a051c87432ca78fe49ac48e494455c87607
Aryan Jha <jhaaryan2004@gmail.com>
2024-07-13 00:56:33 +0530

    Setup RoomDB and done initial setups

---
+++ app/src/main/java/com/example/contactapp/data/Contact.kt
@@ -0,0 +1,17 @@
+package com.example.contactapp.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contacts_table")
+data class Contact(
+
+    @PrimaryKey(autoGenerate = true)var id: Int,
+    @ColumnInfo(name = "user_name")var name: String,
+    var number: String,
+    var email: String,
+    var dateOfCreation : Long,
+    var isActive: Boolean
+
+)
